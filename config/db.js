619dc75aee28d5507938a0aba732857f0b4fecbf
jacklincao <1287075856@qq.com>
2018-05-25 20:29:17 +0800

    添加nodeman、连接数据库

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql'
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user:'root',
+    password: 'root',
+    database: 'node'
+})
+
+function query(sql, callback) {
+    pool.getConnection(function(err, connection) {
+        connection.query(sql, function(err, rows) {
+            callback(err, rows);
+            connection.release();
+        })
+    })
+}
+
+exports.query = query;
--- controller/student/student.js
+++ controller/student/student.js
@@ -1,10 +1,27 @@
+import db from '../../config/db'
+
 class StudentHandler {
   constructor() {
     this.save = this.save.bind(this)
   }
 
   save(req, res, next) {
-    res.send({msg: '暂未实现此接口'})
+    let student = req.body;
+    console.log(student)
+    try {
+      let sql = 'insert into student (name, age, birth) values ("' + student.name + '","' + student.age + '","' + student.birth + '")';
+      console.log(sql);
+      db.query(sql,function(err, rows) {
+        if(err) {
+          res.send('新增失败');
+        }
+        else {
+          res.send(rows);
+        }
+      })
+    } catch (err) {
+      throw err;
+    }
   }
 }
 export default new StudentHandler()
