186dd37944b0726eefb870f2b494212c717c6d7a
jacklincao <1287075856@qq.com>
2018-05-28 17:30:52 +0800

    修改swagger配置

--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,44 @@ import StudentHandler from '../controller/student/student'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * definitions:
+ *   Student:
+ *     properties:
+ *       name:
+ *         type: string
+ *       age:
+ *         type: integer
+ *       idCard:
+ *         type: string
+ *       birth:
+ *         type: string
+ *         format: date-time
+ */
+
+ /**
+  * @swagger
+  * /student/save:
+  *   post:
+  *     tage:
+  *       - Student
+  *     description: 新增学生
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: student
+  *         description: Student object
+  *         in: body
+  *         required: true
+  *         schema: 
+  *           $ref: '#/definitions/Student'
+  *     responses:
+  *       200:
+  *         description: 新增成功
+  *         schema:
+  *           $ref: '#/definitions/Student'
+  */
 router.post('/save', StudentHandler.save)
 
 export default router
